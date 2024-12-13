6a13ee73a168b756b6d41928fa9b5341cb80df5e
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-14 01:04:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstWord(s) {
   // your code here
+	let str = "";
+	if(s.includes(" ")){
+		str = s.trim()
+		let arr = str.split(" ")
+		return arr[0]
+	}else{
+		return s;
+	}
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:"); 
 alert(firstWord(s));
